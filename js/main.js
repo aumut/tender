2fbc2ed7992db0045e563d841eb7648f33396dbe
a_umut <askar.umut.kal@gmail.com>
2020-03-20 20:36:09 +0600

    стили для адаптации

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+// menu mobile
+    $('#btn_gamburg').click(function () {
+        $('.header__nav').toggleClass('active');
+    });
+    $('#btn_close').click(function () {
+        $('.header__nav').toggleClass('active');
+    });
+});
