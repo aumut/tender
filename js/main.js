451793891fc8a639f352c077580d14ed6366686e
a_umut <askar.umut.kal@gmail.com>
2020-03-20 23:13:03 +0600

    mission block is added; feature block style is updated;

--- js/main.js
+++ js/main.js
@@ -6,4 +6,20 @@ $(function() {
     $('#btn_close').click(function () {
         $('.header__nav').toggleClass('active');
     });
+
+//slider
+    let wwidth= $(window).width();
+    //console.log('wwidth = '+ wwidth+ '; cond = '+ (wwidth<768));
+    if (wwidth<768 )
+    {
+        $('.mission__info').slick({
+            dots: true,
+            autoplay: true,
+            arrows: false,
+            adaptiveHeight: true,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1
+        });
+    }
 });
