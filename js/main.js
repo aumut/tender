f95c02d5f37e4414e9f204bbf7b37445f512f06b
a_umut <askar.umut.kal@gmail.com>
2020-03-21 07:03:02 +0600

    all styles are updated; mail send appl is added

--- js/main.js
+++ js/main.js
@@ -9,7 +9,6 @@ $(function() {
 
 //slider
     let wwidth= $(window).width();
-    //console.log('wwidth = '+ wwidth+ '; cond = '+ (wwidth<768));
     if (wwidth<768 )
     {
         $('.mission__info').slick({
@@ -22,4 +21,50 @@ $(function() {
             slidesToShow: 1
         });
     }
+
+//tender sum
+    let tender_count = document.getElementById("tender_count");
+    let tender_sum = document.getElementById("tender_sum");
+
+    tender_count.value = 1105;
+    tender_count.sum = 0;
+
+    let tender_count_value = tender_count.value;
+    let tender_sum_value = tender_count_value * 1055;
+
+    tender_count.innerHTML = formatDigit(tender_count_value);
+    tender_sum.innerHTML = formatDigit(tender_sum_value);
+
+    function formatDigit(n){
+        var s = String(n);
+        var k = s.indexOf(".");
+        if (k < 0) {
+            k = s.length;
+        } else {
+            s += "00"
+        }
+
+        s = s.substr(0, k + 3);
+        for (var i = k - 3, j = n < 0 ? 1 : 0; i > j; i -= 3) s = s.substr(0, i) + " " + s.substr(i);
+
+        return s;
+
+    }
+
+//current date
+    const monthNames = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+    function GetTodayDate() {
+        var tdate = new Date();
+        var dd = tdate.getDate(); // day
+        var MM = monthNames[tdate.getMonth()]; // month
+        var yyyy = tdate.getFullYear(); // year
+        var currentDate= dd + "." + MM + "." + yyyy;
+        return currentDate;
+    }
+    let cur_date =  document.getElementById("cur_date");
+    cur_date.innerHTML = GetTodayDate();
+
+// phone field
+    $(".send_user_phone").mask("+999 (999)99-99-99");
+
 });
