197748785cf19e86a410a2ef45b720f85b503bc7
a_umut <askar.umut.kal@gmail.com>
2020-05-04 18:51:54 +0600

    форма

--- js/main.js
+++ js/main.js
@@ -101,4 +101,52 @@ $(function() {
         e.preventDefault();
         $('html, body').animate({scrollTop:0}, '300');
     });
+
+    //*******************************************************
+
+    function senFormData(name, url) {
+        var formData = new FormData(document.forms[name]);
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            console.log("onready", this.responseText, this.statusText);
+            if (this.readyState == 4 && this.status == 200) {
+                console.log("ready");
+            }
+        }
+        xhr.open("POST", url);
+        xhr.send(formData);
+    }
+
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        $(".send_user_phonef").inputmask("\\9\\96999999999");
+        var validation = Array.prototype.filter.call(forms, function(form) {
+
+            form.addEventListener('submit', function(event) {
+                if ((form.checkValidity() === false)){
+                    event.preventDefault();
+                    event.stopPropagation();
+                } else {
+                    if (form.getAttribute('id') === "send_appl_form") {
+                        senFormData('send_appl_form', "http://api.tender-smart.com/request/v1/user/create/");
+                        event.preventDefault();
+                        $("#link_form").removeClass("d-none");
+                        $("#send_form_wrap").addClass("d-none");
+                    }
+                    if (form.getAttribute('id') === "appl_form") {
+                        senFormData('appl_form', "http://api.tender-smart.com/request/v1/service/mail/send/");
+                    }
+                    if (form.getAttribute('id') === "appl_form_1") {
+                        senFormData('appl_form_1', "http://api.tender-smart.com/request/v1/service/mail/send/");
+                    }
+                }
+                form.classList.add('was-validated');
+            }, false);
+
+        });
+
+    }, false);
+
 });
